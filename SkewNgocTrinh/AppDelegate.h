8871b4ed647a5826f4ba27ac522e00d2e3daad48
DangVuDuy <vuduy081293@gmail.com>
2015-12-22 07:40:53 +0000

    Initial Commit

---
+++ SkewNgocTrinh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SkewNgocTrinh
+//
+//  Created by Duy Dang on 12/22/15.
+//  Copyright © 2015 Duy Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
